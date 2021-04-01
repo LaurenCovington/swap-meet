412b8cae08ab8e07364410208308b66557bf3039
Lauren <lacovington16@gmail.com>
2021-04-01 16:08:10 -0700

    first wave completed, all tests passed - LC git log q quit() exit() ''

---
+++ swap_meet/vendor.py
@@ -0,0 +1,17 @@
+class Vendor:
+    def __init__(self, inventory=None):
+        if inventory == None: 
+            self.inventory = []
+        else:
+            self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item) # remove()?
+            return item
+        else:
+            return False
